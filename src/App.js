7eba54df9df359ecef8b8b2bd298be38c79d8d65
Ceci Mermoz <ceciliamermoz@cliengo.com>
2021-03-04 17:51:11 -0300

    added a custom theme skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Button } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        blebleble
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      
+        <header className="App-header">
+          blebleble
+        </header>
+        <Button variant="outlined" color="primary">Click me</Button>
+    
+    </ThemeProvider>
   );
 }
 
