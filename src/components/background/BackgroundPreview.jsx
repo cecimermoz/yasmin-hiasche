e41597534950704ca2747880df499f7aab900057
cecimermoz <cecii.mermoz@gmail.com>
2022-05-16 15:50:59 -0300

    styling desktop

---
+++ src/components/background/BackgroundPreview.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+//import { makeStyles } from '@material-ui/core'
+import BackgroundSlider from 'react-background-slider'
+//import { breaks } from '../themes/generalTheme'
+
+
+/*const useStyles = makeStyles(() => ({
+}))*/
+
+const BackgroundPreview = () => {
+  // const { backgroundWrapper, gradient, pic, black } = useStyles()
+    
+  const arrayBackgroundPics = [
+    'https://firebasestorage.googleapis.com/v0/b/yasmin-hiasche-web.appspot.com/o/16711626_10210666486833501_3082815882271337721_n.jpg?alt=media&token=212cd664-4f55-4984-b912-d540792dd229',
+    'https://firebasestorage.googleapis.com/v0/b/yasmin-hiasche-web.appspot.com/o/779.jpg?alt=media&token=a2ab745b-c5b0-4907-8987-0aedbad234fb',
+    'https://firebasestorage.googleapis.com/v0/b/yasmin-hiasche-web.appspot.com/o/ANIMA-30.jpg?alt=media&token=3a8579b6-0485-4bde-b5f4-8aa9aea2b5b3',
+    'https://firebasestorage.googleapis.com/v0/b/yasmin-hiasche-web.appspot.com/o/ANIMA-44.jpg?alt=media&token=cb84c3c3-0947-4183-81be-8b799dda5249',
+    'https://firebasestorage.googleapis.com/v0/b/yasmin-hiasche-web.appspot.com/o/760.jpg?alt=media&token=2abff369-79a7-4144-90aa-35ba0845c795',
+    'https://firebasestorage.googleapis.com/v0/b/yasmin-hiasche-web.appspot.com/o/455.jpg?alt=media&token=bf092a44-d603-4fbf-827f-0b372f642e3f',
+    'https://firebasestorage.googleapis.com/v0/b/yasmin-hiasche-web.appspot.com/o/72336011_10218312532899874_5572839878967689216_o.jpg?alt=media&token=8e8d5746-7035-47a2-969f-aa3f263277bd',
+    'https://firebasestorage.googleapis.com/v0/b/yasmin-hiasche-web.appspot.com/o/ANIMA-44.jpg?alt=media&token=f0745174-01c8-47a8-bf4e-1fe756fd9f36',
+  ].sort(() => 0.5 - Math.random());
+
+  return(
+    <BackgroundSlider images={arrayBackgroundPics} duration={4} transition={1} />
+  )
+}
+
+export default BackgroundPreview;
--- src/components/preview/logoAndCTA.jsx
+++ src/components/preview/logoAndCTA.jsx
@@ -1,8 +1,9 @@
 import { makeStyles } from '@material-ui/core';
 import React, { useContext } from 'react';
 import { WebData } from '../../context/dataContext';
+import Firulete from '../main/util/Firulete';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   logoWrapper:{
     display: 'flex',
     justifyContent: 'center',
@@ -12,25 +13,65 @@ const useStyles = makeStyles(() => ({
   },
   logo:{
     height: '20vh',
-    width: '30%',
+    width: '35%',
     backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/yasmin-hiasche-web.appspot.com/o/Estudio%20de%20DanzasYasmin%20Hiasche-1.png?alt=media&token=ed44cac9-462a-4cc8-930c-fd00ccaff737)',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'contain', 
   },
-  button:{
+  tittle:{
+    fontWeight: 400,
+    fontFamily: 'Nunito Sans, sans-serif',
+    textTransform: 'uppercase',
+    color: theme.palette.secondary.main,
+    fontSize: '20px',
   },
-
+  firuleteGeneral:{
+    width: '15%',
+    height: '100%',
+    position: 'absolute',
+    left: '-18%',
+    top: '30%',
+  },
+  firuleteRight:{
+    right: '-18%',
+    left: 'auto'
+  },
+  button:{
+    border: 0,
+    fontSize: '16px',
+    width: '70%',
+    borderRadius: '15px',
+    maxWidth: '180px',
+    padding: '10px 15px',
+    marginTop: '30px',
+    fontWeight: 700,
+    background: `linear-gradient(90deg, ${theme.palette.primary050} 0%, ${theme.palette.ceci050} 100%)`,
+    textTransform: 'uppercase',
+    color: theme.palette.secondary.main,
+    cursor: 'pointer',
+    '&:hover':{
+      background: `linear-gradient(90deg, ${theme.palette.primary.main} 0%, ${theme.palette.ceci} 100%)`,
+    }
+  }
 }))
 
 const LogoCTA = () => {
-  const { button, logo, logoWrapper } = useStyles()
+  const { button, logo, logoWrapper, tittle, firuleteGeneral, firuleteRight } = useStyles()
   const { handleBackgroundPreview } = useContext(WebData)
-
 
   return (
     <div className={logoWrapper} >
       <div className={logo} />
-      <button className={button} onClick={handleBackgroundPreview} >alkalal</button>
+      <div style={{position: 'relative'}}>
+        <Firulete customClasses={{general: firuleteGeneral, rigth: firuleteRight}}>
+          <h1 className={tittle}>
+            Egresada de la escuela de <strong>saida</strong>
+            <br />
+            integrante del ballet <strong>rakkasah</strong>
+          </h1>
+        </Firulete>  
+      </div>
+      <button className={button} onClick={handleBackgroundPreview} >Ingresar</button>
     </div>
   );
 }
